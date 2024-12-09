b407ab537a9508acdbcc0a588b0f9e66376bf677
Monicah Koina <monicahkoina1@gmail.com>
2024-12-09 06:41:12 +0300

    home page

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -8,7 +8,40 @@ class HomePage extends StatelessWidget{
    return const Scaffold(
      body: Column(
        children: [
-
+         Padding(
+           padding: EdgeInsets.only( top: 100.0),
+           child: Text("Frontend Developer"),
+         ),
+         SizedBox(height: 30,),
+         CircleAvatar(radius: 80,),
+         SizedBox(height: 30),
+         Text("Monicah Koina"),
+         SizedBox(height: 30),
+         Column(
+           children: [
+             Row(
+               mainAxisAlignment: MainAxisAlignment.center,
+               children: [
+                 Icon(Icons.phone ),
+                 Text("+254712345678")
+               ],
+             ),
+             Row(
+               mainAxisAlignment: MainAxisAlignment.center,
+               children: [
+                 Icon(Icons.email),
+                 Text("monicahkoina1@gmail.com")
+               ],
+             ),
+             Row(
+               mainAxisAlignment: MainAxisAlignment.center,
+               children: [
+                 Icon(Icons.location_city),
+                 Text("Nairobi, Kenya")
+               ],
+             )
+           ],
+         )
        ],
      ),
    );
